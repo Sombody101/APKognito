ce025d9ee77c837a61211d95ac7c5bc4635e442c
Sombody101 <evansheyborne@gmail.com>
2024-10-13 12:27:55 -0600

    Static Main override for CLI argument parsing

---
+++ APKognito/MainOverride.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace APKognito;
+
+// Is it a bad idea to do this? Maybe.
+// Does that have any influence on what I'm doing? Well, it is here, isn't it?
+internal static class MainOverride
+{
+    [DllImport("kernel32", SetLastError = true)]
+    private static extern bool AttachConsole(int dwProcessId);
+
+    /// <summary>
+    /// Application Entry Point.
+    /// </summary>
+    [STAThread]
+    public static int Main(string[] args)
+    {
+        // Check basic input argument first (mostly used for auto publish script)
+        if (args.Any(arg => arg is "--version" or "-v"))
+        {
+            if (!AttachConsole(-1))
+            {
+                // No console handle from the parent
+                return 5012;
+            }
+
+            Console.WriteLine(Assembly.GetExecutingAssembly()?.GetName()?.Version?.ToString() ?? "[Null]");
+            return 0;
+        }
+
+        APKognito.App app = new APKognito.App();
+        app.InitializeComponent();
+        app.Run();
+
+        return 0;
+    }
+}
