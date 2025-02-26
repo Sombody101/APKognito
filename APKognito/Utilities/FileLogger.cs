001e9a19f8d424b802d0b5d3115dbe028596bfa5
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-02-25 23:25:38 -0700

    Added 'ANY' LogLevel, and log colors class (for LogViewerPage).

--- APKognito/Utilities/FileLogger.cs
+++ APKognito/Utilities/FileLogger.cs
@@ -11,12 +11,16 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Documents;
+using System.Windows.Media;
+using Wpf.Ui.Appearance;
 
 namespace APKognito.Utilities;
 
 public enum LogLevel
 {
-    INFO,
+    ANY = int.MaxValue,
+
+    INFO = 0,
     WARNING,
     ERROR,
     FATAL,
@@ -29,10 +33,10 @@ public enum LogLevel
 /// </summary>
 public static class FileLogger
 {
-    public const string TimeFormatString = "hh:mm:ss.fff tt: ";
+    public const string TimeFormatString = "hh:mm:ss.fff tt:";
     public const string ReplacementUsername = "[:USER:]";
 
-    private static readonly object lockObject = new();
+    private static readonly object _lock = new();
     private static readonly string logFilePath = Path.Combine(App.AppDataDirectory!.FullName, "applog.log");
     private static readonly string exceptionLogFilePath = Path.Combine(App.AppDataDirectory!.FullName, "exlog.log");
 
@@ -68,10 +72,10 @@ public static class FileLogger
             : "\n";
 
         string admin = MainWindowViewModel.LaunchedAsAdministrator
-            ? "[ADMIN]"
+            ? " ADMIN"
             : string.Empty;
 
-        string logEntry = $"[{UtcTime}{logLevel.ToString().ToUpper()}] {admin}\t[{GetCallerInfo()}] {text}{newline}";
+        string logEntry = $"[{UtcTime} {logLevel.ToString().ToUpper()}{admin}]    [{GetCallerInfo()}] {text}{newline}";
         LogGenericFinal(logEntry);
     }
 
@@ -96,18 +100,33 @@ public static class FileLogger
         LogGenericFinal(log.ToString(), ex);
     }
 
-    public static void Log(string log)
+    public static void Log(string log, bool ignore = false)
     {
+        if (ignore)
+        {
+            return;
+        }
+
         LogGeneric(log, LogLevel.INFO);
     }
 
-    public static void LogWarning(string log)
+    public static void LogWarning(string log, bool ignore = false)
     {
+        if (ignore)
+        {
+            return;
+        }
+
         LogGeneric(log, LogLevel.WARNING);
     }
 
-    public static void LogError(string log)
+    public static void LogError(string log, bool ignore = false)
     {
+        if (ignore)
+        {
+            return;
+        }
+
         LogGeneric(log, LogLevel.ERROR);
     }
 
@@ -116,8 +135,13 @@ public static class FileLogger
         LogGeneric(log, LogLevel.FATAL);
     }
 
-    public static void LogFatal(Exception exception)
+    public static void LogFatal(Exception exception, bool ignore = false)
     {
+        if (ignore)
+        {
+            return;
+        }
+
         LogGeneric($"StackTrace added to exlog: {exception.GetType().Name}: {exception.Message}", LogLevel.FATAL);
         LogGenericException(exception, "[LogFatal added message: Fatal exception]");
     }
@@ -132,8 +156,13 @@ public static class FileLogger
         LogGenericException(exception);
     }
 
-    public static void LogException(Exception exception)
+    public static void LogException(Exception exception, bool ignore = false)
     {
+        if (ignore)
+        {
+            return;
+        }
+
         LogGenericException(exception);
     }
 
@@ -147,7 +176,7 @@ public static class FileLogger
         string[] filesToPack = [
             logFilePath,
             exceptionLogFilePath,
-            ConfigurationFactory.GetConfigInfo<RenameSessionList>().GetCompletePath()
+            ConfigurationFactory.Instance.GetConfigInfo<RenameSessionList>().GetCompletePath()
         ];
 
         string packPath = Path.Combine(App.AppDataDirectory!.FullName, "logpack");
@@ -204,13 +233,28 @@ public static class FileLogger
         return outputPack;
     }
 
+    public static Brush LogLevelToBrush(LogLevel level)
+    {
+        return level switch
+        {
+            LogLevel.INFO       => LogLevelColors.Info,
+            LogLevel.WARNING    => LogLevelColors.Warning,
+            LogLevel.ERROR      => LogLevelColors.Error,
+            LogLevel.FATAL      => LogLevelColors.Fatal,
+            LogLevel.DEBUG      => Brushes.Cyan,
+            LogLevel.TRACE      => LogLevelColors.Trace,
+
+            _ => throw new ArgumentException($"No color mapping made for log level '{level}'")
+        };
+    }
+
     private static void LogGenericFinal(string entry, Exception? ex = null)
     {
         try
         {
             entry = entry.Redact();
 
-            lock (lockObject)
+            lock (_lock)
             {
                 if (ex is null)
                 {
@@ -231,43 +275,48 @@ public static class FileLogger
 
     private static string GetCallerInfo()
     {
-        StackTrace stackTrace = new(true);
+        const int initialFrameDepth = 2;
+        const int maxFrames = 10;
 
-        if (stackTrace.FrameCount >= 3)
+        Type fileLoggerType = typeof(FileLogger);
+        Type loggableObservableObjectType = typeof(LoggableObservableObject);
+
+        StackTrace stackTrace = new();
+
+        for (int frameDepth = initialFrameDepth; frameDepth < stackTrace.FrameCount
+            && frameDepth < initialFrameDepth + maxFrames; frameDepth++)
         {
-            int frameDepth = 2;
+            StackFrame? frame = stackTrace.GetFrame(frameDepth);
+            MethodBase? method = frame?.GetMethod();
 
-        Retry:
-            StackFrame frame = stackTrace.GetFrame(frameDepth)!;
-            MethodBase? method = frame!.GetMethod();
-
-            // Prevents Log aliases from being selected as the caller
-            if (method?.DeclaringType == typeof(FileLogger) || method?.DeclaringType == typeof(LoggableObservableObject))
+            if (method is null
+                || method.DeclaringType is null
+                || method.DeclaringType == fileLoggerType
+                || method.DeclaringType == loggableObservableObjectType)
             {
-                ++frameDepth;
-                goto Retry;
+                continue;
             }
 
             string className;
             string methodName;
 
-            if (typeof(IAsyncStateMachine).IsAssignableFrom(method?.DeclaringType))
+            if (typeof(IAsyncStateMachine).IsAssignableFrom(method.DeclaringType))
             {
                 // Fix async methods (<AwaitedMethodName>d__10.MoveNext -> RealClassName.AwaitedMethodName)
-                className = method.DeclaringType.DeclaringType?.Name ?? "[Unknown]";
+                className = method.DeclaringType.DeclaringType?.Name ?? "[Unknown.IAsyncClass]";
                 methodName = method.DeclaringType.Name.TrimStart('<');
                 methodName = methodName[0..methodName.IndexOf('>')];
             }
             else
             {
-                className = method?.DeclaringType?.Name ?? "[Unknown]";
-                methodName = method?.Name ?? "[Unknown]";
+                className = method.DeclaringType.Name ?? "[Unknown.Class]";
+                methodName = method.Name ?? "[Unknown.Method]";
             }
 
             return $"{className}.{methodName}";
         }
 
-        return $"[Failed to get caller info: ";
+        return "Failed to get caller info";
     }
 
     private static string GetFormattedException(Exception ex)
@@ -278,5 +327,51 @@ public static class FileLogger
             .AppendLine(ex.StackTrace ?? "[NO STACK]");
 
         return exception.ToString();
+    }
+
+    public static class LogLevelColors
+    {
+        static LogLevelColors()
+        {
+            PopulateColors(ApplicationThemeManager.GetAppTheme() is ApplicationTheme.Dark);
+
+            ApplicationThemeManager.Changed += (sender, e) =>
+            {
+                PopulateColors(sender is ApplicationTheme.Dark);
+            };
+        }
+
+        public static Brush Info { get; private set; }
+        public static Brush Warning { get; private set; }
+        public static Brush Error { get; private set; }
+        public static Brush Fatal { get; private set; }
+        public static Brush Debug { get; private set; }
+        public static Brush Trace { get; private set; }
+
+        public static SolidColorBrush ToBrush(string value)
+        {
+            return (SolidColorBrush)new BrushConverter().ConvertFromString(value)!;
+        }
+
+        private static void PopulateColors(bool dark = true)
+        {
+            Error = ToBrush("#F44336");
+
+            if (dark)
+            {
+                Info = ToBrush("#006d9e");
+                Warning = ToBrush("#FF9800");
+                Fatal = ToBrush("#FF5722");
+                Debug = ToBrush("#009688");
+                Trace = ToBrush("#3F51B5");
+                return;
+            }
+
+            Info = ToBrush("#5BB6FF");
+            Warning = ToBrush("#FF9800");
+            Fatal = ToBrush("#FF7043");
+            Debug = ToBrush("#009688");
+            Trace = ToBrush("#3F51B5");
+        }
     }
 }
