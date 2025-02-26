2d439e8a2fe461e46f3a0c9514ef0fc476413c67
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-02-25 23:09:19 -0700

    Added 'ApkEditorToolPaths' for global access.

---
+++ APKognito/Utilities/ApkEditorToolPaths.cs
@@ -0,0 +1,8 @@
+﻿namespace APKognito.Utilities;
+
+internal static class ApkEditorToolPaths
+{
+    public static string ApktoolJarPath { get; set; }
+    public static string ApktoolBatPath { get; set; }
+    public static string ApksignerJarPath { get; set; }
+}
