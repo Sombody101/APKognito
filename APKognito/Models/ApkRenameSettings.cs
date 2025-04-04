5eb69a5e79f23ddae2f2482732619e6555a6bc7d
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-04-03 23:55:25 -0600

    Moved 'ApkRenameSettings' to Models

---
+++ APKognito/Models/ApkRenameSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace APKognito.Models;
+
+public sealed class ApkRenameSettings
+{
+    public string SourceApkPath { get; set; } = string.Empty;
+
+    public string OutputDirectory { get; init; } = string.Empty;
+    public string JavaPath { get; init; } = string.Empty;
+    public string TempDirectory { get; init; } = string.Empty;
+    public string ApkReplacementName { get; init; } = string.Empty;
+
+    public Action<string>? OnPackageNameFound { get; init; }
+}
