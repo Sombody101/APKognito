6d900a3f9ce60f94ea1245a08d205e2ed4e574bd
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-04-12 03:15:43 -0600

    Added IProgressReporter

---
+++ APKognito/Utilities/MVVM/IProgressReporter.cs
@@ -0,0 +1,23 @@
+﻿namespace APKognito.Utilities.MVVM;
+
+public interface IProgressReporter
+{
+    public event EventHandler<ProgressUpdateEventArgs> ProgressChanged;
+
+    void ReportUpdate(string update, UpdateType updateType = UpdateType.Content);
+
+    void ForwardUpdate(ProgressUpdateEventArgs args);
+}
+
+public class ProgressUpdateEventArgs(string update, UpdateType updateType) : EventArgs
+{
+    public string UpdateValue { get; } = update;
+
+    public UpdateType UpdateType { get; } = updateType;
+}
+
+public enum UpdateType
+{
+    Content,
+    Title,
+}
