a4b3c00698249ebe67cc102da9d7cb8bd5c6e707
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-06-20 02:04:39 -0600

    Remove dangling backup

--- ApkMod/ApkEditorToolPaths.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace APKognito.ApkMod;
-
-internal static class ApkEditorToolPaths
-{
-    public static string ApktoolJarPath { get; set; } = string.Empty;
-    public static string ApktoolBatPath { get; set; } = string.Empty;
-    public static string ApksignerJarPath { get; set; } = string.Empty;
-    public static string ZipalignPath { get; set; } = string.Empty;
-}
