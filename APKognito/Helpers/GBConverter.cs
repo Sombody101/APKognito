baf5c27313f3ef23ef78e54174ee2c1d27a68c11
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-07-23 02:51:00 -0600

    Added ulong overload

--- APKognito/Helpers/GBConverter.cs
+++ APKognito/Helpers/GBConverter.cs
@@ -7,9 +7,12 @@ public class GBConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        return value is long totalUsedSpace 
-            ? FormatSizeFromBytes(totalUsedSpace) 
-            : value;
+        return value switch
+        {
+            ulong ul => FormatSizeFromBytes(ul),
+            long l => FormatSizeFromBytes(l),
+            _ => value
+        };
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
@@ -18,6 +21,17 @@ public class GBConverter : IValueConverter
     }
 
     public static string FormatSizeFromBytes(long size)
+    {
+        return size switch
+        {
+            >= (1024 * 1024 * 1024) => $"{size / 1024f / 1024f / 1024f:0.00} GB",
+            >= (1024 * 1024) => $"{size / 1024f / 1024f:n0} MB",
+            >= 1024 => $"{size / 1024f:n0} KB",
+            _ => $"{size} bytes"
+        };
+    }
+
+    public static string FormatSizeFromBytes(ulong size)
     {
         return size switch
         {
