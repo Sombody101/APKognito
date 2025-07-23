2c9da714ce36c6b9527ba2d3b420e4d0d0fee974
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-07-23 02:46:35 -0600

    Renamed 'KognitoConfig' to 'RenameConfiguration' to match its purpose

---
+++ APKognito/Configurations/ConfigModels/RenameConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace APKognito.Configurations.ConfigModels;
+
+[ConfigFile("rename-settings.json", ConfigType.Json, ConfigModifiers.JsonIndented | ConfigModifiers.JsonIgnoreMissing)]
+public sealed record class _RenameConfiguration : IKognitoConfig
+{
+
+}
