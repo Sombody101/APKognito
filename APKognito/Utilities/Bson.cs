733b2cce4d6efec4bd7ee8dd73abdaabca928c84
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-07-23 03:00:41 -0600

    Added BSON de/serializer

---
+++ APKognito/Utilities/Bson.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+
+namespace APKognito.Utilities;
+
+internal static class Bson
+{
+    public static byte[] Serialize<T>(T obj)
+    {
+        using MemoryStream ms = new();
+        using BsonDataWriter writer = new(ms);
+
+        JsonSerializer serializer = new();
+        serializer.Serialize(writer, obj);
+
+        return ms.ToArray();
+    }
+
+    public static T? Deserialize<T>(byte[] data)
+    {
+        using MemoryStream stream = new(data);
+        using BsonDataReader reader = new(stream);
+
+        var deserializer = new JsonSerializer();
+        return deserializer.Deserialize<T>(reader);
+    }
+}
