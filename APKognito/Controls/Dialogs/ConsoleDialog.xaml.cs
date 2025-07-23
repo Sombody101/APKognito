19d9edee1973e23fe844833fc2dac1429eff51dc
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-07-23 02:48:54 -0600

    Added dialog to act as a mock console for single-use commands

---
+++ APKognito/Controls/Dialogs/ConsoleDialog.xaml.cs
@@ -0,0 +1,66 @@
+﻿using APKognito.Utilities.MVVM;
+using APKognito.ViewModels.Pages;
+using Wpf.Ui.Controls;
+
+namespace APKognito.Controls.Dialogs;
+
+public sealed partial class ConsoleDialog
+{
+    public ConsoleDialogViewModel ViewModel { get; }
+
+    public ConsoleDialog(string command, ContentPresenter? contentPresenter)
+        : base(contentPresenter)
+    {
+        ViewModel = new ConsoleDialogViewModel(command);
+        DataContext = this;
+
+        InitializeComponent();
+    }
+
+    protected override void OnLoaded()
+    {
+        _ = ViewModel.StartCommandAsync();
+        base.OnLoaded();
+    }
+
+    private async void ContentDialog_Closed(ContentDialog sender, ContentDialogClosedEventArgs args)
+    {
+        await ViewModel.CancelCommandCommand.ExecuteAsync(null);
+        ViewModel.Dispose();
+    }
+
+    public sealed partial class ConsoleDialogViewModel : LoggableObservableObject, IDisposable
+    {
+        private readonly string _command;
+
+        private readonly CancellationTokenSource _cancellationTokenSource;
+
+        [ObservableProperty]
+        public partial string InteractionButtonText { get; set; } = "Cancel install";
+
+        public ConsoleDialogViewModel(string command)
+        {
+            _command = command;
+            _cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        public async Task StartCommandAsync()
+        {
+            var token = _cancellationTokenSource.Token;
+            await Task.Run(async () => await AdbConsoleViewModel.RunBuiltinCommandAsync(_command, this, token), token);
+
+            InteractionButtonText = "Close";
+        }
+
+        [RelayCommand]
+        private async Task OnCancelCommandAsync()
+        {
+            await _cancellationTokenSource.CancelAsync();
+        }
+
+        public void Dispose()
+        {
+            _cancellationTokenSource?.Dispose();
+        }
+    }
+}
