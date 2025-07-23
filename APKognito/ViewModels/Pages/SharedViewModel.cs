dd5aab8364e291e378a53030e6a5700cbf05c5d6
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-07-23 03:04:13 -0600

    Added a shared VM for disabling controls when renaming packages

---
+++ APKognito/ViewModels/Pages/SharedViewModel.cs
@@ -0,0 +1,9 @@
+﻿using APKognito.Utilities.MVVM;
+
+namespace APKognito.ViewModels.Pages;
+
+public sealed partial class SharedViewModel : ViewModel, IViewable
+{
+    [ObservableProperty]
+    public partial bool ConfigurationControlsEnabled { get; set; } = true;
+}
