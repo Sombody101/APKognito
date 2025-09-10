83f403337d593b45e04a0b87290f3dbf7ad0d292
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-09-10 17:04:44 -0600

    Added a theme config

---
+++ APKognito/Configurations/ConfigModels/UserThemeConfig.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Wpf.Ui.Appearance;
+
+namespace APKognito.Configurations.ConfigModels;
+
+[ConfigFile("usertheme.json", ConfigType.Json, ConfigModifiers.JsonIndented | ConfigModifiers.JsonIgnoreMissing)]
+public sealed record UserThemeConfig : IKognitoConfig
+{
+    [JsonProperty("theme")]
+    public ApplicationTheme AppTheme { get; set; } = ApplicationThemeManager.GetAppTheme();
+
+    [JsonProperty("use_system_accent")]
+    public bool UseSystemAccent { get; set; } = true;
+
+    public void ApplyUserTheme()
+    {
+        ApplicationThemeManager.Apply(AppTheme);
+
+        if (UseSystemAccent)
+        {
+            ApplicationAccentColorManager.ApplySystemAccent();
+        }
+    }
+}
