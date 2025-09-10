49495929b22d9c116a5f02e1d7ef8e001451e1f3
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-09-10 17:05:09 -0600

    Gave cache model new property names, and added logging options

--- APKognito/Configurations/ConfigModels/CacheStorage.cs
+++ APKognito/Configurations/ConfigModels/CacheStorage.cs
@@ -1,4 +1,7 @@
-﻿namespace APKognito.Configurations.ConfigModels;
+﻿using APKognito.Utilities;
+using Newtonsoft.Json;
+
+namespace APKognito.Configurations.ConfigModels;
 
 [ConfigFile("misc.cache", ConfigType.Bson)]
 internal record CacheStorage : IKognitoConfig
@@ -6,19 +9,27 @@ internal record CacheStorage : IKognitoConfig
     /// <summary>
     /// Holds old APK paths to load (at least so there's content to present).
     /// </summary>
+    [JsonProperty("asp")]
     public string? ApkSourcePath { get; set; }
 
     /// <summary>
     /// Specifies where to open the FileDialog to select an APK.
     /// </summary>
+    [JsonProperty("ldd")]
     public string? LastDialogDirectory { get; set; } = AppDomain.CurrentDomain.BaseDirectory;
 
     /// <summary>
     /// The newest update location (Stored here as a poor attempt of preventing user tampering)
     /// </summary>
+    [JsonProperty("usl")]
     public string? UpdateSourceLocation { get; set; }
 
-    public CacheStorage()
-    {
-    }
+    [JsonProperty("ifl")]
+    public bool IsFirstLaunch { get; set; } = true;
+
+    [JsonProperty("mll")]
+    public LogLevel MinimumLogLevel { get; set; } = LogLevel.INFO;
+
+    [JsonProperty("letv")]
+    public bool LogExceptionsToView { get; set; } = false;
 }
