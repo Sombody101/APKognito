7b3f33d751ad782db510931c1c3b029732aa4e38
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-11-22 16:30:16 -0700

    Change tray battery layout

--- APKognito/Models/AndroidDevice.cs
+++ APKognito/Models/AndroidDevice.cs
@@ -4,6 +4,8 @@ public record AndroidDevice
 {
     public static readonly AndroidDevice Empty = new();
 
+    public string DeviceName { get; init; } = string.Empty;
+
     public int BatteryLevel { get; init; } = -1;
 
     public float FreeSpace { get; init; } = 0;
