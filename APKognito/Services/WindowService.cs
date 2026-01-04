121b3cf5fc4cc67b468ba4f3205d1ddad351caec
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2026-01-04 13:40:42 -0700

    Add "window service" for easier window-dialog usage

---
+++ APKognito/Services/WindowService.cs
@@ -0,0 +1,27 @@
+﻿using APKognito.Models;
+using APKognito.Utilities.MVVM;
+using APKognito.ViewModels.Windows;
+using APKognito.Views.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace APKognito.Services;
+
+public interface IWindowService
+{
+    JavaDownloadInfo? PromptJavaInstallationWindow(IViewLogger logger);
+}
+
+public sealed class WindowService(IServiceProvider serviceProvider) : IWindowService
+{
+    public JavaDownloadInfo? PromptJavaInstallationWindow(IViewLogger logger)
+    {
+        JavaInstallationSelectionViewModel viewmodel = ActivatorUtilities.CreateInstance<JavaInstallationSelectionViewModel>(serviceProvider, logger);
+        JavaInstallationSelectionWindow window = ActivatorUtilities.CreateInstance<JavaInstallationSelectionWindow>(serviceProvider, viewmodel);
+
+        window.Owner = Application.Current.MainWindow;
+
+        _ = window.ShowDialog();
+
+        return window.DialogResult;
+    }
+}
